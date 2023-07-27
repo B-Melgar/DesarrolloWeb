0e120ff709fa37e18450705117ed4b2f3c4e8045
Alex Melgar <melgarbryan778@gmail.com>
2023-07-27 00:16:04 -0600

    primer repositorio

---
+++ Actividad02/formulario.js
@@ -0,0 +1,32 @@
+<script>
+  function validarFormulario() {
+    // Validar Datos Generales
+    const nombre = document.getElementById('nombre').value;
+    const fechaNacimiento = document.getElementById('fechaNacimiento').value;
+    const direccion = document.getElementById('direccion').value;
+    const telefono = document.getElementById('telefono').value;
+    const sexo = document.getElementById('sexo').value;
+
+    if (!nombre || !fechaNacimiento || !direccion || !telefono || !sexo) {
+      alert('Por favor, complete todos los campos de Datos Generales.');
+      return false;
+    }
+
+    // Validar Datos de Pago con Tarjeta
+    const numTarjeta = document.getElementById('numTarjeta').value;
+    const titularTarjeta = document.getElementById('titularTarjeta').value;
+    const fechaExpiracion = document.getElementById('fechaExpiracion').value;
+    const cvv = document.getElementById('cvv').value;
+
+    if (!numTarjeta || !titularTarjeta || !fechaExpiracion || !cvv) {
+      alert('Por favor, complete todos los campos de Datos de Pago con Tarjeta.');
+      return false;
+    }
+
+    // Aquí podrías realizar validaciones más específicas sobre los datos ingresados
+    // como longitud, formato, etc.
+
+    alert('¡Formulario enviado correctamente!');
+    return true;
+  }
+</script>
